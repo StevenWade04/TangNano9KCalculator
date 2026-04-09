ee369745fb83a722878828d61bdf795b922cf04b
StevenWade04 <stevenwade2004@gmail.com>
2026-04-09 16:42:55 +0930

    addition and subtraction optimization
    Removed function from operation.v
    This function resulted in blowout in synthesis time.
    Now reduced.
    Tested and verified on hardware.

--- VerilogCode/operation.v
+++ VerilogCode/operation.v
@@ -1,8 +1,8 @@
 module operation(
     input wire clk,
     input wire calcWait,
-    input wire [24:0] numberA,
-    input wire [24:0] numberB,
+    input wire [31:0] numberA,
+    input wire [31:0] numberB,
     input wire [1:0] operationFlag,
     output reg done,
     output reg [24:0] binOut
@@ -13,6 +13,7 @@ localparam ADDITION = 3'd1;
 localparam SUBTRACTION = 3'd2;
 localparam MULTIPLICATION = 3'd3;
 localparam DIVISION = 3'd4;
+localparam CONVERT = 3'd5;
 
 localparam passedAdd = 2'd0;
 localparam passedSub = 2'd1;
@@ -20,19 +21,11 @@ localparam passedMul = 2'd2;
 localparam passedDiv = 2'd3;
 
 reg [2:0] state = 0;
-
-function [24:0] convert_bcd_to_bin;
-    input [31:0] bcd;
-    integer i;
-    reg [24:0] temp;
-    begin
-        temp = 0;
-        for (i = 7; i >= 0; i = i - 1) begin
-            temp = temp * 10 + bcd[4*i +: 4];
-        end
-        convert_bcd_to_bin = temp;
-    end
-endfunction
+reg [1:0] opperand = 0;
+reg [24:0] binA;
+reg [24:0] binB;
+reg [3:0] counter;
+reg [3:0] digitA, digitB;
 
 always @(posedge clk) begin
     case (state)
@@ -40,20 +33,50 @@ always @(posedge clk) begin
             done <= 0;
             if (calcWait) begin
                 case (operationFlag)
-                    passedAdd: state <= ADDITION;
-                    passedSub: state <= SUBTRACTION;
-                    passedMul: state <= MULTIPLICATION;
-                    passedDiv: state <= DIVISION;
+                    passedAdd: begin
+                        state <= CONVERT;
+                        opperand <= passedAdd;
+                    end
+                    passedSub: begin
+                        state <= CONVERT;
+                        opperand <= passedSub;
+                    end
+                    passedMul: begin
+                        state <= CONVERT;
+                        opperand <= passedMul;
+                    end
+                    passedDiv: begin
+                        state <= CONVERT;
+                        opperand <= passedDiv;
+                    end
                 endcase
+            end else begin 
+                opperand <= 0;
+                counter <= 0;
+                counterPrev <= 0;
+                binA <= 0;
+                binB <= 0;
+            end
+        end
+        CONVERT: begin
+            if (counter == 8) state <= opperand + 1'b1;
+            else begin
+                digitA = numberA[31 - counter*4 -: 4];
+                digitB = numberB[31 - counter*4 -: 4];
+
+                binA <= (binA << 3) + (binA << 1) + digitA;
+                binB <= (binB << 3) + (binB << 1) + digitB;
+
+                counter <= counter + 1;
             end
         end
         ADDITION: begin
-            binOut <= convert_bcd_to_bin(numberA) + convert_bcd_to_bin(numberB);
+            binOut <= binA + binB;
             done <= 1;
             state <= WAITING;
         end
         SUBTRACTION: begin
-            binOut <= convert_bcd_to_bin(numberA) - convert_bcd_to_bin(numberB);
+            binOut <= binA - binB;
             done <= 1;
             state <= WAITING;
         end
